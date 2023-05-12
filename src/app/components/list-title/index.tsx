bd492b06b05fcf91c658fb3d69e766c3de0c7122
Caique Giovannini <caique_giovannini@hotmail.com>
2023-05-12 00:04:11 -0300

    Create app base structure

---
+++ src/app/components/list-title/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.scss'
+
+interface ListTitleProps {
+    title: string
+}
+
+export function ListTitle({title}: ListTitleProps) {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>{title}</h1>
+        </div>
+    )
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import { ListTitle } from './components/list-title'
+import { List } from './components/list'
+import { AddNewButton } from './components/add-new-button'
+import styles from './Home.module.scss'
+
 export default function Home() {
   return (
-    <main>
-      <h1>HELLO TODO APP</h1>      
-    </main>
+    <section className={styles.container}>
+      <header>
+        <ListTitle title='Lista de tarefas' />
+      </header>
+      <main>
+        <List />
+      </main>
+      <footer>
+        <AddNewButton />
+      </footer>
+    </section>
   )
 }
