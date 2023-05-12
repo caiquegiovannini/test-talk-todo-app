cf2d8965bf5d4c6c4c542240a73873f55f672fee
Caique Giovannini <caique_giovannini@hotmail.com>
2023-05-12 00:02:41 -0300

    Config paths and imports aliases

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack(config) {
+        config.resolve.alias = {
+          ...config.resolve.alias,
+          "colors.scss": path.resolve(__dirname, "./src/styles/colors.scss"),
+        }
+        return config
+      }
+}
 
 module.exports = nextConfig
