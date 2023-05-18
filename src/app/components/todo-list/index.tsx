ff53de68ab5f24c03e619aeb7060c16dd8ab52c2
Caique <caique_giovannini@hotmail.com>
2023-05-17 23:56:15 -0300

    Create ToDoList component

--- src/app/components/todo-list/index.tsx
+++ src/app/components/todo-list/index.tsx
@@ -1,7 +1,35 @@
+'use client'
+
+import { useState } from 'react'
+import { ToDoItem } from '../todo-item'
 import styles from './styles.module.scss'
 
+interface ToDoData {
+    id: string,
+    description: string
+}
+
 export function ToDoList() {
+    const [todos, setTodos] = useState<ToDoData[]>([])
+    const [todo, setTodo] = useState({id: '123', description: 'Comprar cafe', checked: false}) // mock
+
+    function handleCheckToDo() {
+        setTodo(current => ({
+            ...current,
+            checked: !current.checked,
+        }))
+    }
+
     return (
-        <main></main>
+        <main>
+            <ul>
+                {/* {todos.length > 0 && (
+                    todos.map(todo => (
+                        <ToDoItem key={todo.id} todo={todo} />
+                    ))
+                )} */}
+                <ToDoItem todo={todo} onCheckToDo={handleCheckToDo} />
+            </ul>
+        </main>
     )
 };
